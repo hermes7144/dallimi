fa296fffc99225724a0cf23a21e038ec69c879b9
hermes7144 <kichancho@gmail.com>
2025-03-11 21:11:00 +0900

    test

--- src/service/notification.ts
+++ src/service/notification.ts
@@ -76,12 +76,3 @@ export const sendFCMTokenToServer = async (id: string, token: string, deviceType
   }
 };
 
-onMessage(messaging, (payload) => {
-  console.log("포그라운드 메시지 수신:", payload);
-
-  // 알림 표시 (서비스 워커가 아닌 클라이언트에서)
-  new Notification('title', {
-    body: 'body',
-    icon: ''
-  });
-});
