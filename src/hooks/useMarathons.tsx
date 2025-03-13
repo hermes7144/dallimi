0318485b597354eaed05ebcbdac79dc7e3f1f6ff
hermes7144 <kichancho@gmail.com>
2025-03-13 16:44:31 +0900

    feat:refresh test

--- src/hooks/useMarathons.tsx
+++ src/hooks/useMarathons.tsx
@@ -5,7 +5,7 @@ const fetcher = (url: string) => fetch(url).then((res) => res.json());
 export default function useMarathons() {
   const { data: marathons, isLoading, error, mutate } =useSWR<Marathon[]>('/api/marathons', fetcher, {
     revalidateOnMount: true, // 마운트될 때마다 새로 요청
-    // revalidateOnFocus: true, // 페이지 포커스 시 자동 리프레시
+    revalidateOnFocus: true, // 페이지 포커스 시 자동 리프레시
     revalidateOnReconnect: true,
   });
   return { marathons, isLoading, error };
