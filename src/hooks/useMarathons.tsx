565e6bf4e29270100758240df5078ae23f970a97
hermes7144 <kichancho@gmail.com>
2025-03-13 16:42:03 +0900

    feat:marathon type 제거한거 다시추가

--- src/hooks/useMarathons.tsx
+++ src/hooks/useMarathons.tsx
@@ -3,7 +3,7 @@ import useSWR from 'swr';
 const fetcher = (url: string) => fetch(url).then((res) => res.json());
 
 export default function useMarathons() {
-  const { data: marathons, isLoading, error, mutate } =useSWR('/api/marathons', fetcher, {
+  const { data: marathons, isLoading, error, mutate } =useSWR<Marathon[]>('/api/marathons', fetcher, {
     revalidateOnMount: true, // 마운트될 때마다 새로 요청
     // revalidateOnFocus: true, // 페이지 포커스 시 자동 리프레시
     revalidateOnReconnect: true,
