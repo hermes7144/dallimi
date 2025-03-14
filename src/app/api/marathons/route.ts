9c53629c77730523c4354444caaa8ebf0416ec1d
hermes7144 <kichancho@gmail.com>
2025-03-14 10:47:35 +0900

    feat:force-dynamic

--- src/app/api/marathons/route.ts
+++ src/app/api/marathons/route.ts
@@ -3,6 +3,7 @@ import { authOptions } from '../auth/[...nextauth]/route';
 import { getMarathons } from '@/service/marathon';
 import { NextResponse } from 'next/server';
 
+export const dynamic = 'force-dynamic'; // 🔹 항상 최신 데이터 가져오기
 
 export async function GET() {
   // const session = await getServerSession(authOptions);
