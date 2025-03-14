68aa368d316deb685f8f3c5a5202a24dd75be974
hermes7144 <kichancho@gmail.com>
2025-03-14 14:05:15 +0900

    feat:allim 테스트

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -14,31 +14,27 @@ const messaging = firebase.messaging();
 
 self.addEventListener('push', function(event) {
   const message = event.data.json();  // FCM 메시지
-  console.log('message',message);
+  console.log('message', message);
+
   const title = message.notification.title;
   const body = message.notification.body;
-  // const icon = message.data.icon;
-
-  const currentOrigin = self.location.origin;
-  // const clickAction = currentOrigin === 'https://runal.netlify.app'
-  //   ? 'https://runal.netlify.app/' :'https://dev-runal.netlify.app/';
-
   const clickAction = 'https://dallimi.vercel.app/';
 
   const options = {
     body,
-    // notificationclick 있으면?
     data: {
       click_action: clickAction,
     },
-    // 배지, 아이콘?
-
-    // icon: icon, 
     icon: '/icons/favicon-32x32.png', 
     badge: '/icons/favicon-32x32.png', 
     vibrate: [200, 100, 200],  // 진동 패턴
     timestamp: Date.now(),
   };
+
+  // 중복 메시지 방지
+  if (event.notification) {
+    event.notification.close();  // 기존 알림이 있는 경우 닫기
+  }
 
   event.waitUntil(
     self.registration.showNotification(title, options)
@@ -47,7 +43,6 @@ self.addEventListener('push', function(event) {
 
 self.addEventListener('notificationclick', function(event) {
   event.notification.close();  // 알림을 닫습니다.
-  console.log('event', event)
   const clickAction = event.notification.data.click_action;
   
   event.waitUntil(
