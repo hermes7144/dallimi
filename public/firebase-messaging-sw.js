107c6d8783e3bc9f23aa25af8f682d89423b2004
hermes7144 <kichancho@gmail.com>
2025-03-14 14:17:13 +0900

    feat:temp

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -12,40 +12,40 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-// self.addEventListener('push', function(event) {
-//   const message = event.data.json();  // FCM 메시지
-//   console.log('message', message);
+self.addEventListener('push', function(event) {
+  const message = event.data.json();  // FCM 메시지
+  console.log('message', message);
 
-//   const title = message.notification.title;
-//   const body = message.notification.body;
-//   const clickAction = 'https://dallimi.vercel.app/';
-
-//   const options = {
-//     body,
-//     data: {
-//       click_action: clickAction,
-//     },
-//     icon: '/icons/favicon-32x32.png', 
-//     badge: '/icons/favicon-32x32.png', 
-//     vibrate: [200, 100, 200],  // 진동 패턴
-//     timestamp: Date.now(),
-//   };
-
-//   // 중복 메시지 방지
-//   if (event.notification) {
-//     event.notification.close();  // 기존 알림이 있는 경우 닫기
-//   }
-
-//   event.waitUntil(
-//     self.registration.showNotification(title, options)
-//   );
-// });
-
-self.addEventListener('notificationclick', function(event) {
-  event.notification.close();  // 알림을 닫습니다.
-  // const clickAction = event.notification.data.click_action;
+  const title = message.notification.title;
+  const body = message.notification.body;
   const clickAction = 'https://dallimi.vercel.app/';
+
+  const options = {
+    body,
+    data: {
+      click_action: clickAction,
+    },
+    icon: '/icons/favicon-32x32.png', 
+    badge: '/icons/favicon-32x32.png', 
+    vibrate: [200, 100, 200],  // 진동 패턴
+    timestamp: Date.now(),
+  };
+
+  // 중복 메시지 방지
+  if (event.notification) {
+    event.notification.close();  // 기존 알림이 있는 경우 닫기
+  }
+
   event.waitUntil(
-    clients.openWindow(clickAction)
+    self.registration.showNotification(title, options)
   );
 });
+
+// self.addEventListener('notificationclick', function(event) {
+//   event.notification.close();  // 알림을 닫습니다.
+//   const clickAction = event.notification.data.click_action;
+  
+//   event.waitUntil(
+//     clients.openWindow(clickAction)
+//   );
+// });
