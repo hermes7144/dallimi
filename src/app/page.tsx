80be6d531c99ff59f11bcdcc4d82ee0532f5e932
hermes7144 <kichancho@gmail.com>
2025-03-26 11:34:30 +0900

    feat:site-verification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,6 @@ import MarathonList from '@/components/MarathonList';
 
 export default function Home() {
   return (
-
-  // <section className='w-full flex p-4 justify-center'>
     <MarathonList />
-  // </section>
-
   )
 }
