1d5762dc080e5bfb1cfaf3a2932dcd14f285f968
hermes7144 <kichancho@gmail.com>
2025-04-18 14:28:11 +0900

    feat:마라톤 서치 페이지 wip

--- src/components/MarathonSearch.tsx
+++ src/components/MarathonSearch.tsx
@@ -17,14 +17,14 @@ export default function MarathonSearch() {
   };
 
   return (
-    <section className='w-full max-w-2xl my-4 flex flex-col items-center'>
+    <section className='w-full my-4 flex flex-col items-center m-3'>
       <form className='w-full mb-4' onSubmit={onSubmit}>
-        <input className='w-full text-xl p-3 outline-none border border-gray-400' autoFocus type='text' placeholder='Search for a username or name' value={keyword} onChange={(e) => setKeyword(e.target.value)} />
+        <input className='w-full text-xl p-3 outline-none border border-gray-400' autoFocus type='text' placeholder='마라톤을 검색해주세요' value={keyword} onChange={(e) => setKeyword(e.target.value)} />
       </form>
       {error && <p>무언가가 잘못 되었음</p>}
       {isLoading && <GridSpninner/>}
       {!isLoading && !error && marathons?.length === 0 && <p>찾는 대회가 없음</p>}
-      <ul className='grid grid-cols-1 lg:grid-cols-2 gap-4 p-4'>
+      <ul className='grid grid-cols-1 lg:grid-cols-4 gap-4 p-4 w-full'>
       {marathons && marathons.map(marathon => <li key={marathon.id}>
           <MarathonListCard marathon={marathon} priority={false} />
         </li>)}
