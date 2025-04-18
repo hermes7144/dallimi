b6b99d030c4dca63fb9e33c51781458561f0f8a8
hermes7144 <kichancho@gmail.com>
2025-04-18 09:37:57 +0900

    feat:sourcedir 추가

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,6 +3,7 @@ module.exports = {
   siteUrl: 'https://runal.vercel.app/',
   generateRobotsTxt: true,
   sitemapSize: 5000,
+  sourceDir: 'app', 
   changefreq: "daily",
   priority: 1,
   exclude: [],
