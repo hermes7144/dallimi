6739a458edd8fcbf3f9383cb49bb98cf3be5634e
hermes7144 <kichancho@gmail.com>
2025-05-15 14:22:50 +0900

    feat:sitemap 재생성

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,16 +1,32 @@
 import type { MetadataRoute } from 'next'
+import { getMarathons } from '@/service/marathon'
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = process.env.NEXT_PUBLIC_SITE_URL || 'https://runal.vercel.app'
+
+  const marathons = await getMarathons()
+
+  const marathonPages: MetadataRoute.Sitemap  = marathons.map((marathon) => ({
+    url: `${baseUrl}/marathon/${marathon.id}`,
+    lastModified: new Date(), // 적절한 날짜 필드 사용
+    changeFrequency: 'daily',
+    priority: 0.8,
+  }))
 
   const staticPages: MetadataRoute.Sitemap = [
     {
       url: `${baseUrl}`,
       lastModified: new Date(),
       changeFrequency: 'daily',
-      priority: 1.0
+      priority: 1.0,
+    },
+    {
+      url: `${baseUrl}/marathon`,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 0.9,
     },
   ]
 
-  return staticPages
+  return [...staticPages, ...marathonPages]
 }
